16019ab6080cc00c1b8e0b5ec566deb0fd3f2c40
senzoo-oh <osj3418@gmail.com>
2024-11-03 02:10:49 +0900

    database: 🗄️ 도메인 수정

---
+++ src/main/java/com/nuri/nuribackend/repository/ChatMessageRepository.java
@@ -0,0 +1,16 @@
+package com.nuri.nuribackend.repository;
+
+import com.nuri.nuribackend.domain.ChatMessage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
+
+    @Query(value = "{ 'chatId' : ?0, 'msgType' : ?1 }")
+    List<ChatMessage> findByChatIdAndMsgType(Integer chatId, String msgType);
+}
--- src/main/java/com/nuri/nuribackend/service/SampleService.java
+++ src/main/java/com/nuri/nuribackend/service/SampleService.java
@@ -1,12 +1,12 @@
 package com.nuri.nuribackend.service;
 
+import com.nuri.nuribackend.domain.Chat;
 import com.nuri.nuribackend.domain.ChatMessage;
 import com.nuri.nuribackend.domain.Feedback.Feedback;
 import com.nuri.nuribackend.domain.Feedback.FeedbackContent;
 import com.nuri.nuribackend.domain.Feedback.MonthlyFeedback;
-import com.nuri.nuribackend.repository.ChatRepository;
-import com.nuri.nuribackend.repository.FeedbackRepository;
-import com.nuri.nuribackend.repository.MonthlyFeedbackRepository;
+import com.nuri.nuribackend.domain.User;
+import com.nuri.nuribackend.repository.*;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -18,44 +18,86 @@ public class SampleService {
 
     private final FeedbackRepository feedbackRepository;
     private final MonthlyFeedbackRepository monthlyFeedbackRepository;
+    private final ChatMessageRepository chatMessageRepository;
     private final ChatRepository chatRepository;
+    private final UserRepository userRepository;
 
     public SampleService(FeedbackRepository feedbackRepository,
                          MonthlyFeedbackRepository monthlyFeedbackRepository,
-                         ChatRepository chatRepository) {
+                         ChatMessageRepository chatMessageRepository,
+                         ChatRepository chatRepository,
+                         UserRepository userRepository) {
         this.feedbackRepository = feedbackRepository;
         this.monthlyFeedbackRepository = monthlyFeedbackRepository;
+        this.chatMessageRepository = chatMessageRepository;
         this.chatRepository = chatRepository;
+        this.userRepository = userRepository;
+    }
+
+    User user = new User();
+    Chat chat = new Chat();
+    ChatMessage chatMessage = new ChatMessage();
+    Feedback feedback = new Feedback();
+    MonthlyFeedback monthlyFeedback = new MonthlyFeedback();
+
+    public void saveUser() {
+
+        user.setUserId("nuri");
+        user.setEmail("nuri@gmail.com");
+        user.setPassword("nuri");
+        user.setName("nuri");
+        user.setAge(24);
+
+        userRepository.save(user);
+    }
+    public void saveChat() {
+
+        chat.setDate(new Date());
+        chat.setChatId(1);
+        chat.setSubject("채팅방 제목");
+        chat.setUserMsgCnt(10);
+        chat.setSummary("채팅방 요약");
+        chat.setUser(user);
+
+        chatRepository.save(chat);
+    }
+
+    public void saveChatMessage() {
+
+        chatMessage.setChatId(chat.getChatId());
+        chatMessage.setMsgType("user");
+        chatMessage.setMsgText("안녕 GPT야! 한국어 배우고 싶어!");
+        chatMessage.setMsgSound("S3 URL");
+
+        chatMessageRepository.save(chatMessage); // MongoDB에 저장
     }
 
     public void saveFeedbackSample() {
-        Feedback feedback = new Feedback();
-        feedback.setFeedbackId("1");
-        feedback.setMsgId("msg123");
+
+        feedback.setMsgId(chatMessage.getId());
 
         FeedbackContent grammar = new FeedbackContent();
-        grammar.setContent("Grammar feedback");
+        grammar.setContent("문법에 대한 피드백");
         feedback.setGrammar(grammar);
 
         FeedbackContent vocabulary = new FeedbackContent();
-        vocabulary.setContent("Vocabulary feedback");
+        vocabulary.setContent("어휘에 대한 피드백");
         feedback.setVocabulary(vocabulary);
 
         FeedbackContent ageInGroup = new FeedbackContent();
-        ageInGroup.setContent("Age group feedback");
+        ageInGroup.setContent("연령별 어휘에 대한 피드백");
         feedback.setAgeInGroup(ageInGroup);
 
         FeedbackContent formalInformal = new FeedbackContent();
-        formalInformal.setContent("Formality feedback");
+        formalInformal.setContent("경어체에 대한 피드백");
         feedback.setFormalInformal(formalInformal);
 
         feedbackRepository.save(feedback); // MongoDB에 저장
     }
 
     public void saveMonthlyFeedbackSample() {
-        MonthlyFeedback monthlyFeedback = new MonthlyFeedback();
-        monthlyFeedback.setFeedbackId("1");
-        monthlyFeedback.setUserId("1");
+
+        monthlyFeedback.setUserId(user.getId());
         monthlyFeedback.setDate(new Date());
 
         FeedbackContent grammar = new FeedbackContent();
@@ -75,17 +117,5 @@ public class SampleService {
         monthlyFeedback.setFormalInformal(formalInformal);
 
         monthlyFeedbackRepository.save(monthlyFeedback); // MongoDB에 저장
-    }
-
-    public void saveChat() {
-        ChatMessage chat = new ChatMessage();
-        chat.setMsgId("12345");
-        chat.setChatId(1);
-        chat.setMsgType("user");
-        chat.setMsgText("안녕하세요");
-        chat.setMsgSound("S3 URL");
-
-
-        chatRepository.save(chat); // MongoDB에 저장
     }
 }
