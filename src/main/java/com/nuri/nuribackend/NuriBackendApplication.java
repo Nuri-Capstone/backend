6af4da50a7abc1f53a12c32ebfa89c396df1efde
vvzvvv <tmdgusv@gmail.com>
2024-11-17 17:06:14 +0900

    feat:✨ GPT API 호출 및 응답

--- src/main/java/com/nuri/nuribackend/NuriBackendApplication.java
+++ src/main/java/com/nuri/nuribackend/NuriBackendApplication.java
@@ -2,6 +2,8 @@ package com.nuri.nuribackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NuriBackendApplication {
@@ -9,5 +11,8 @@ public class NuriBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(NuriBackendApplication.class, args);
     }
-
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
