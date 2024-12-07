e41f48f1259fc59c7d461712801ec8b80f481b47
jaehuiLee <90444611+jaehuiLee@users.noreply.github.com>
2024-12-08 02:30:05 +0900

    feat: ✨ 사용자 순위 기능 추가

--- src/main/java/com/nuri/nuribackend/service/RankingService.java
+++ src/main/java/com/nuri/nuribackend/service/RankingService.java
@@ -7,10 +7,14 @@ import com.nuri.nuribackend.dto.Feedback.RankingDto;
 import com.nuri.nuribackend.exception.CustomException;
 import com.nuri.nuribackend.repository.ChatRepository;
 import com.nuri.nuribackend.repository.RankingRepository;
+import com.nuri.nuribackend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +22,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class RankingService {
     private final RankingRepository rankingRepository;
+    private final UserRepository userRepository;
+
     @Transactional
     public RankingDto addRanking(RankingDto rankingDto) {
         try {
@@ -29,5 +35,32 @@ public class RankingService {
             log.error("Error adding ranking: {}", ex.getMessage());
             throw new CustomException(ex.getErrorCode(), ex.getMessage());
         }
+    }
+
+    public int getAllUser() {
+        List<Long> userIds = userRepository.findAllUserIds();
+        int allUser = userIds.size();
+
+        return allUser;
+    }
+
+    public int getUserRanking(Long userId) {
+        // Long: userId, Integer: msgCnt
+        List<Long> userIds = userRepository.findAllUserIds();
+
+        List<Object[]> results = rankingRepository.getMsgCnt(userIds);
+        // 내림차순 정렬
+        results.sort((o1, o2) -> Long.compare((Long) o2[1], (Long) o1[1]));
+        // rank를 Integer(msgCnt)를 기준으로 내림차순 정렬
+
+        // key(userID)를 조회함
+        int userRanking = -1; // 순위 초기값
+        for (int i = 0; i < results.size(); i++) {
+            if (results.get(i)[0].equals(userId)) {
+                userRanking = i + 1; // 순위는 인덱스 + 1
+                break;
+            }
+        }
+        return userRanking;
     }
 }
